4b60370ba034f3c56cdabca2dfcbf6792d730680
2503485-BSBI <2503485@students.au.edu.pk>
2025-12-14 11:01:10 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// 
+function openLogin() {
+    document.getElementById("acc_created").style.display = "block";
+    document.body.style.overflow = "hidden";
+}
+
+function closeLogin() {
+    document.getElementById("acc_created").style.display = "none";
+    document.body.style.overflow = "auto";
+}
